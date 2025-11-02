e9bc8b0bd63eadb08a21b24b818a853d54c1d8e2
Sharjeel Yunus <sharjeel924@gmail.com>
2025-11-03 01:59:23 +0500

    Refactor nextra initialization for improved compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const withNextra = require('nextra')({
+const nextraRaw = require('nextra');
+const nextra = typeof nextraRaw === 'function' ? nextraRaw : nextraRaw.default || nextraRaw;
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
-})
+});
 
 module.exports = withNextra({
   async redirects() {
