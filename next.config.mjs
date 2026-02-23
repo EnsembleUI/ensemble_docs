9ae7ffcf757ab78d5ead63570d17bc01c97ad09f
Sharjeel Yunus <sharjeel924@gmail.com>
2026-02-24 04:06:51 +0500

    Update configuration and add wrangler.jsonc for deployment; include 'out' directory in .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import nextra from 'nextra'
 const withNextra = nextra({})
 
 export default withNextra({
+  output: 'export',
+  images: { unoptimized: true },
   async redirects() {
     return [
       {
